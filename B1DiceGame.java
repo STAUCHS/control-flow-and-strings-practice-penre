e09ee658f1c8d1b2cd3234aca0bce3704b978be3
penre <Tianyou.zhang25@ycdsbk12.ca>
2024-04-02 19:44:39 -0400

    ww

--- B1DiceGame.java
+++ B1DiceGame.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 class B1DiceGame extends ConsoleProgram {
 
   /**
@@ -6,7 +8,41 @@ class B1DiceGame extends ConsoleProgram {
   */
   
   public void run() {
+  
+  // random object
+
+  Random diceroll = new Random();
+
+  // declare variables
+
+  int intDiceOne;
+  int intDiceTwo;
+
+  // roll 100 times
+
+  for (int dice = 0; dice > 100; dice++){
+    intDiceOne = diceroll.nextInt(1,6);
+    intDiceTwo = diceroll.nextInt(1,6);
+
+  // if statements
+
+  if(intDiceOne + intDiceTwo == 2){
+    System.out.println("snake eyes ");
+
+  }
+  
+  if(intDiceOne + intDiceTwo == 7){
+    System.out.println("lucky seven ");
+
+
+
+  }
+
+
+
+    
     
   }
+}
 }
 
--- B2GuessingGame.java
+++ B2GuessingGame.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 class B2GuessingGame extends ConsoleProgram {
 
   /**
@@ -6,6 +8,37 @@ class B2GuessingGame extends ConsoleProgram {
   */
   
   public void run() {
+  
+  // declare random number and variable
+
+    Random secretnumber = new Random();
+    
+    int intRandomNumber;
+    int intGuess;
+
+  // generate random number from 1 to 100
+
+    intRandomNumber = secretnumber.nextInt(100,1);
+
+  // guess random number
+    intGuess = readInt("What is the random number? ");
+
+    if(intGuess > intRandomNumber){
+      System.out.println("too high");
+
+    }
+
+    if(intGuess < intRandomNumber){
+      System.out.println("too low");
+
+    }
+    
+    if(intGuess == intRandomNumber){
+      System.out.println("congratulations ");
+    }
+
+    
+  
     
   }
 }
