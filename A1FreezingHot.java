c3dc6a1d0556c234101480a23e9632c6b797124d
penre <Tianyou.zhang25@ycdsbk12.ca>
2024-04-02 16:38:25 -0400

    w

--- A1FreezingHot.java
+++ A1FreezingHot.java
@@ -6,6 +6,31 @@ class A1FreezingHot extends ConsoleProgram {
   */
   
   public void run() {
+  
+    int intTemp1;
+    int intTemp2;
+
+  // get user input
+    intTemp1 = readInt("What is the first temperature? ");
+    intTemp2 = readInt("What is the second temperature? ");
+
+    System.out.println(intTemp1);
+    System.out.println(intTemp2);
+
+  // true or false 
+    if(intTemp1 > 100){
+      System.out.println("true ");
+    }
+    else{
+      System.out.println("false ");
+    }
     
+    if(intTemp2 < 0){
+      System.out.println("True ");
+    }
+    else{
+      System.out.println("False ");
+    }
+
   }
 }
--- A2HasTeen.java
+++ A2HasTeen.java
@@ -6,6 +6,26 @@ class A2HasTeen extends ConsoleProgram {
   */
   
   public void run() {
-    
+    int intAge1;
+    int intAge2;
+    int intAge3;
+
+  // get user input
+    intAge1 = readInt("What is the first age ? ");
+    intAge2 = readInt("What is the second age ? ");
+    intAge3 = readInt("What is the third age ? ");
+
+    System.out.println(intAge1);
+    System.out.println(intAge2);
+    System.out.println(intAge3);
+
+  // true or false age
+    if((intAge1 <= 19 && intAge1 >= 13) || (intAge2 <= 19 && intAge2 >= 13) || (intAge3 <= 19 && intAge3 >= 13)){
+      System.out.println("True ");
+    }
+    else{
+      System.out.println("False ");
+    }
+
   }
 }
